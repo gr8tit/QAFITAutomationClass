6d3d0487c51fb77f1f204f83bb43eb0b37d32f48
temitopeoyedemi <temmygr8t@gmail.com>
2025-05-03 21:06:15 +0200

    Extent Reporter

---
+++ src/main/java/util/ExtentManager.java
@@ -0,0 +1,23 @@
+package util;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+
+public class ExtentManager {
+
+    private static ExtentReports extent;
+
+    public static ExtentReports getInstance() {
+        if (extent == null) {
+            String reportPath = System.getProperty("user.dir") + "/test-output/ExtentReport.html";
+            ExtentSparkReporter spark = new ExtentSparkReporter(reportPath);
+            extent = new ExtentReports();
+            extent.attachReporter(spark);
+
+            extent.setSystemInfo("OS", System.getProperty("os.name"));
+            extent.setSystemInfo("Java Version", System.getProperty("java.version"));
+        }
+        return extent;
+    }
+}
--- src/test/java/org/QAFITAutomationClass2025/testSauceDemo/SauceDemoAddtoCartTest.java
+++ src/test/java/org/QAFITAutomationClass2025/testSauceDemo/SauceDemoAddtoCartTest.java
@@ -1,18 +1,36 @@
 package org.QAFITAutomationClass2025.testSauceDemo;
 
+import com.aventstack.extentreports.AnalysisStrategy;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
 import org.QAFITAutomationClass2025.SauceDemoProject.Pages.SauceDemoPage;
 import org.QAFITAutomationClass2025.TestBase;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 import static org.testng.Assert.assertEquals;
 
 public class SauceDemoAddtoCartTest extends TestBase {
 
+    ExtentReports extent;
+    ExtentTest test;
 
+    @BeforeSuite
+    public void setReport() {
+        ExtentSparkReporter spark = new ExtentSparkReporter("test-output/extent-report.html");
+        extent = new ExtentReports();
+        extent.attachReporter(spark);
+        extent.setSystemInfo("OS", System.getProperty("os.name"));
+    }
 
     @Test(priority = 1, groups = {"smoke", "regression"}, description = "Add to cart Test")
     public void testSauceDemoAddtoCart() throws InterruptedException { //Add to cart Test
+        test = extent.createTest("testSauceDemoAddtoCart");
+        test.info("Add to cart Test started");
+
         //TestSteps
 //
         TestBase testBase = new TestBase();
@@ -28,12 +46,24 @@ public class SauceDemoAddtoCartTest extends TestBase {
 
         sauceDemoPage.getPriceOfBackpack();
 
+        logger.info("Price of backpack is: " + sauceDemoPage.getPriceOfBackpack());
+        test.pass("Test passed");
+        extent.flush();
+
+//
+
+
+
 
     }
 
     @Test
     public void testSauceDemoRemoveFromCart() { //Add to cart Test
         //TestSteps
+
+        test = extent.createTest("testSauceDemoRemoveFromCart");
+        test.info("" + "Remove from cart Test started");
+
         TestBase testBase = new TestBase();
         testBase.setUp("firefox");
         SauceDemoPage sauceDemoPage = new SauceDemoPage();
@@ -44,6 +74,8 @@ public class SauceDemoAddtoCartTest extends TestBase {
         sauceDemoPage.clickAddToCartButton();
         sauceDemoPage.clickCartButton();
         sauceDemoPage.clickRemoveBackpackButton();
+        test.pass("Test passed");
+        extent.flush();
 
 
     }
@@ -51,6 +83,9 @@ public class SauceDemoAddtoCartTest extends TestBase {
     @Test
     public void testSauceDemoContinueShoppingButton() { //Add to cart Test
         //TestSteps
+
+        test = extent.createTest("testSauceDemoContinueShoppingButton");
+        test.info("Continue shopping Test started");
         TestBase testBase = new TestBase();
         testBase.setUp("firefox");
         SauceDemoPage sauceDemoPage = new SauceDemoPage();
@@ -61,6 +96,8 @@ public class SauceDemoAddtoCartTest extends TestBase {
         sauceDemoPage.clickAddToCartButton();
         sauceDemoPage.clickCartButton();
         sauceDemoPage.clickContinueShoppingButton();
+        test.pass("Test passed");
+        extent.flush();
 
     }
 
@@ -68,6 +105,8 @@ public class SauceDemoAddtoCartTest extends TestBase {
     @Test
     public void testSauceDemoCheckout() { //Add to cart Test
         //TestSteps
+        test = extent.createTest("testSauceDemoCheckout");
+        test.info("" + "Checkout Test started");
         TestBase testBase = new TestBase();
         testBase.setUp("firefox");
         SauceDemoPage sauceDemoPage = new SauceDemoPage();
@@ -80,10 +119,16 @@ public class SauceDemoAddtoCartTest extends TestBase {
         sauceDemoPage.clickCheckoutButton();
         assertEquals(sauceDemoPage.getCheckoutPageTitle(), "Checkout: Your Information");
         logger.info("Checkout page title is: " + sauceDemoPage.getCheckoutPageTitle());
+        test.pass("Test passed");
+        logger.info(test.info("Checkout page title is: " + sauceDemoPage.getCheckoutPageTitle()));
+        extent.flush();
     }
 
 
-
+//    @AfterSuite
+//    public void tearDown() {
+//        extent.flush(); // Generates the report
+//    }
 
 
 }
